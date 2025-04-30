4fc98407eca4cd05201774467e7843a7e4c0924d
razeN <71156828+razeNFR@users.noreply.github.com>
2025-04-30 23:34:15 +0200

    Update yggflix_service.py

--- stream_fusion/utils/yggfilx/yggflix_service.py
+++ stream_fusion/utils/yggfilx/yggflix_service.py
@@ -103,7 +103,7 @@ class YggflixService:
                 if result.get("id")
                 else None
             )
-            item.indexer="YGG"
+            item.indexer="Ygg"
             item.seeders=result.get("seeders", 0)
             item.privacy="private"
             item.languages=detect_languages(item.raw_title, default_language="fr")
